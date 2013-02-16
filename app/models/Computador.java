5b2d436ab47bb26218f16d0109b1aaff672a858d
Rodolfo Ferraz <rcaferraz@gmail.com>
2013-02-16 13:39:46 -0300

    app/models/Computador.java: Criando model Computador

---
+++ app/models/Computador.java
@@ -0,0 +1,22 @@
+package models;
+
+import java.util.*;
+
+public class Computador {
+
+	public Long id;
+	public String status;
+
+	public static List<Computador> all() {
+		//TODO Deve retornar a lista de computadores cadastrados
+		// Esta' retornando uma lista vazia.
+		return new ArrayList<Computador>();
+	}
+	
+	public static void create(Computador c) {
+	}
+
+	public static void delete(Computador c) {
+	}
+
+}
